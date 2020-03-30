3069312b310d7df6d089655edbc5710650dbccca
abdelrahmanahmed <abdelrahman.wahdan@outlook.com>
2020-03-30 21:44:25 +0200

    Updates

---
+++ precache-manifest.0268d0d36955d71cde43be013eeb72ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c174341a9fea12251c2caf58a06a2c",
+    "url": "/brandslisten-task/index.html"
+  },
+  {
+    "revision": "2f336f24bdcab919b388",
+    "url": "/brandslisten-task/static/js/2.8a61a9d3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/brandslisten-task/static/js/2.8a61a9d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e3c6e6643418779dbeb",
+    "url": "/brandslisten-task/static/js/main.b05ffbd9.chunk.js"
+  },
+  {
+    "revision": "7c3158d15264482d73a6",
+    "url": "/brandslisten-task/static/js/runtime-main.b1a61028.js"
+  }
+]);
